41244c003fe4f70cfd6e81899f0cff91139b0f28
Mike <vodopyanov@gmail.com>
2012-09-24 12:33:48 +0300

    Gravatar url fix.
    Generated image url doesn't work. Change "&s=32" to "?s=32" is needed.

--- functions.php
+++ functions.php
@@ -129,7 +129,7 @@ function bones_comments($comment, $args, $depth) {
 			    	// create variable
 			    	$bgauthemail = get_comment_author_email();
 			    ?>
-			    <img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5($bgauthemail); ?>&s=32" class="load-gravatar avatar avatar-48 photo" height="32" width="32" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
+			    <img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5($bgauthemail); ?>?s=32" class="load-gravatar avatar avatar-48 photo" height="32" width="32" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
 			    <!-- end custom gravatar call -->
 				<?php printf(__('<cite class="fn">%s</cite>'), get_comment_author_link()) ?>
 				<time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time('F jS, Y'); ?> </a></time>
