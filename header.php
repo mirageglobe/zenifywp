d47a30a704bb3f55a6d3a4f456b69d5521243739
Eddie Machado <machado08@gmail.com>
2013-03-09 14:42:28 -0800

    organized order

--- header.php
+++ header.php
@@ -8,10 +8,10 @@
 	<head>
 		<meta charset="utf-8">
 
-		<title><?php wp_title(''); ?></title>
-
 		<!-- Google Chrome Frame for IE -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title><?php wp_title(''); ?></title>
 
 		<!-- mobile meta (hooray!) -->
 		<meta name="HandheldFriendly" content="True">
