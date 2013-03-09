ad66757f1964392104eaef94c05acebac031422e
Eddie Machado <machado08@gmail.com>
2013-03-09 14:43:11 -0800

    remove the echo
    it's unneeded

--- archive.php
+++ archive.php
@@ -1,40 +1,40 @@
 <?php get_header(); ?>
-			
+
 			<div id="content">
-			
+
 				<div id="inner-content" class="wrap clearfix">
-				
+
 				    <div id="main" class="eightcol first clearfix" role="main">
-				
+
 					    <?php if (is_category()) { ?>
 						    <h1 class="archive-title h2">
 							    <span><?php _e("Posts Categorized:", "bonestheme"); ?></span> <?php single_cat_title(); ?>
 					    	</h1>
-					    
-					    <?php } elseif (is_tag()) { ?> 
+
+					    <?php } elseif (is_tag()) { ?>
 						    <h1 class="archive-title h2">
 							    <span><?php _e("Posts Tagged:", "bonestheme"); ?></span> <?php single_tag_title(); ?>
 						    </h1>
-					    
-					    <?php } elseif (is_author()) { 
+
+					    <?php } elseif (is_author()) {
 					    	global $post;
 					    	$author_id = $post->post_author;
 					    ?>
 						    <h1 class="archive-title h2">
 
-						    	<span><?php _e("Posts By:", "bonestheme"); ?></span> <?php echo the_author_meta('display_name', $author_id); ?>
+						    	<span><?php _e("Posts By:", "bonestheme"); ?></span> <?php the_author_meta('display_name', $author_id); ?>
 
 						    </h1>
 					    <?php } elseif (is_day()) { ?>
 						    <h1 class="archive-title h2">
 	    						<span><?php _e("Daily Archives:", "bonestheme"); ?></span> <?php the_time('l, F j, Y'); ?>
 						    </h1>
-		
+
 		    			<?php } elseif (is_month()) { ?>
 			    		    <h1 class="archive-title h2">
 				    	    	<span><?php _e("Monthly Archives:", "bonestheme"); ?></span> <?php the_time('F Y'); ?>
 					        </h1>
-					
+
 					    <?php } elseif (is_year()) { ?>
 					        <h1 class="archive-title h2">
 					    	    <span><?php _e("Yearly Archives:", "bonestheme"); ?></span> <?php the_time('Y'); ?>
@@ -42,34 +42,34 @@
 					    <?php } ?>
 
 					    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
+
 					    <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-						
+
 						    <header class="article-header">
-							
+
 							    <h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
                   <p class="byline vcard"><?php
                     printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'bonestheme')), bones_get_the_author_posts_link(), get_the_category_list(', '));
                   ?></p>
-						
+
 						    </header> <!-- end article header -->
-					
+
 						    <section class="entry-content clearfix">
-						
+
 							    <?php the_post_thumbnail( 'bones-thumb-300' ); ?>
-						
+
 							    <?php the_excerpt(); ?>
-					
+
 						    </section> <!-- end article section -->
-						
+
 						    <footer class="article-footer">
-							
+
 						    </footer> <!-- end article footer -->
-					
+
 					    </article> <!-- end article -->
-					
-					    <?php endwhile; ?>	
-					
+
+					    <?php endwhile; ?>
+
 					        <?php if (function_exists('bones_page_navi')) { ?>
 						        <?php bones_page_navi(); ?>
 					        <?php } else { ?>
@@ -80,9 +80,9 @@
 							        </ul>
 					    	    </nav>
 					        <?php } ?>
-					
+
 					    <?php else : ?>
-					
+
     					    <article id="post-not-found" class="hentry clearfix">
     						    <header class="article-header">
     							    <h1><?php _e("Oops, Post Not Found!", "bonestheme"); ?></h1>
@@ -94,15 +94,15 @@
     		    				    <p><?php _e("This is the error message in the archive.php template.", "bonestheme"); ?></p>
     			    			</footer>
     				    	</article>
-					
+
 					    <?php endif; ?>
-			
+
     				</div> <!-- end #main -->
-    
+
 	    			<?php get_sidebar(); ?>
-                
+
                 </div> <!-- end #inner-content -->
-                
+
 			</div> <!-- end #content -->
 
 <?php get_footer(); ?>
