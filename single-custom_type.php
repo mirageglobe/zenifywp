43c4c47a06fa89977985044d485bb4367e4b4cf4
David Lowry <dave@twentyfour.local>
2013-03-11 23:10:31 +0000

    Fixed the representation of the datestamp ordinals - capital S instead of small s.

--- single-custom_type.php
+++ single-custom_type.php
@@ -29,7 +29,7 @@ single-bookmarks.php
 
 							    <h1 class="single-title custom-post-type-title"><?php the_title(); ?></h1>
                   <p class="byline vcard"><?php
-                    printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F js, Y', 'bonestheme')), bones_get_the_author_posts_link(), get_the_term_list( $post->ID, 'custom_cat', ' ', ', ', '' ));
+                    printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'bonestheme')), bones_get_the_author_posts_link(), get_the_term_list( $post->ID, 'custom_cat', ' ', ', ', '' ));
                   ?></p>
 
 						    </header> <!-- end article header -->
