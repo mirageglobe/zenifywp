20bd3ce0ff1313ba144bc58f6e7dd59066dc057f
Christian Foellmann <foellmann@foe-services.de>
2013-09-02 20:51:41 +0200

    added flush_rewrite_rules on Theme switch

--- library/custom-post-type.php
+++ library/custom-post-type.php
@@ -14,6 +14,13 @@ Developed by: Eddie Machado
 URL: http://themble.com/bones/
 */
 
+// Flush rewrite rules for custom post types
+add_action( 'after_switch_theme', 'bones_flush_rewrite_rules' );
+
+// Flush your rewrite rules
+function bones_flush_rewrite_rules() {
+	flush_rewrite_rules();
+}
 
 // let's create the function for the custom type
 function custom_post_example() { 
