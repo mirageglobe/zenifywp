a3795dc19de1ffc5b39b2bea5ab36830b3c59d07
Jimmy MG Lim <mirageglobe@gmail.com>
2013-12-25 15:17:29 +0000

    removed page and transferred into index.php file

--- 404.php
+++ 404.php
@@ -1,43 +1,18 @@
 <?php get_header(); ?>
 
-			<div id="content">
-
-				<div id="inner-content" class="wrap clearfix">
-
-					<div id="main" class="eightcol first clearfix" role="main">
-
-						<article id="post-not-found" class="hentry clearfix">
-
-							<header class="article-header">
-
-								<h1><?php _e( 'Epic 404 - Article Not Found', 'bonestheme' ); ?></h1>
-
-							</header>
-
-							<section class="entry-content">
-
-								<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'bonestheme' ); ?></p>
-
-							</section>
-
-							<section class="search">
-
-									<p><?php get_search_form(); ?></p>
-
-							</section>
-
-							<footer class="article-footer">
-
-									<p><?php _e( 'This is the 404.php template.', 'bonestheme' ); ?></p>
-
-							</footer>
-
-						</article>
-
-					</div>
-
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="panel panel-default">
+				<div class="panel-body">
+                    <section class="search">
+                        <h1>Page does not exist <small>404 Error</small></h1>
+                        <p>The page request is not found. Try searching for this.</p>
+                        <p><?php get_search_form(); ?></p>
+                    </section>
 				</div>
-
 			</div>
-
+		</div>
+	</div>
+</div>
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,72 +1,84 @@
 <?php get_header(); ?>
 
-			<div id="content">
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<?php if(is_front_page()){ ?>
+					<!--Front page element here-->
+					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+							<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+						</ol>
 
-				<div id="inner-content" class="wrap clearfix">
-
-						<div id="main" class="eightcol first clearfix" role="main">
-
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
-
-								<header class="article-header">
-
-									<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<p class="byline vcard"><?php
-										printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
-									?></p>
-
-								</header>
-
-								<section class="entry-content clearfix">
-									<?php the_content(); ?>
-								</section>
-
-								<footer class="article-footer">
-									<p class="tags"><?php the_tags( '<span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?></p>
-
-								</footer>
-
-								<?php // comments_template(); // uncomment if you want to use them ?>
-
-							</article>
-
-							<?php endwhile; ?>
-
-									<?php if ( function_exists( 'bones_page_navi' ) ) { ?>
-											<?php bones_page_navi(); ?>
-									<?php } else { ?>
-											<nav class="wp-prev-next">
-													<ul class="clearfix">
-														<li class="prev-link"><?php next_posts_link( __( '&laquo; Older Entries', 'bonestheme' )) ?></li>
-														<li class="next-link"><?php previous_posts_link( __( 'Newer Entries &raquo;', 'bonestheme' )) ?></li>
-													</ul>
-											</nav>
-									<?php } ?>
-
-							<?php else : ?>
-
-									<article id="post-not-found" class="hentry clearfix">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
-
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner">
+							<div class="item active">
+							  	<img src="<?php echo get_template_directory_uri(); ?>/library/images/gmslide1.jpg" alt="gmagarwood">
+							  	<div class="carousel-caption">
+							    	gmagarwood
+							  	</div>
+							</div>
+							<div class="item">
+							  	<img src="<?php echo get_template_directory_uri(); ?>/library/images/gmslide2.jpg" alt="gmagarwood">
+							  	<div class="carousel-caption">
+							    	gmagarwood
+							  	</div>
+							</div>
 						</div>
 
-						<?php get_sidebar(); ?>
+						<!-- Controls -->
+						<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left"></span>
+						</a>
+						<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right"></span>
+						</a>
+					</div>
+					<?php } ?>
 
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+                        <?php if(!is_front_page()){ ?>
+						<header class="article-header">
+							<h2><?php the_title(); ?></h2>
+						</header>
+                        <?php } ?>
+						<section class="entry-content clearfix" itemprop="articleBody">
+							<?php the_content(); ?>
+						</section>
+
+						<footer class="article-footer">
+							<?php the_tags( '<span class="tags">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?>
+						</footer>
+
+						<?php // comments_template(); ?>
+
+					</article>
+
+					<?php endwhile; else : ?>
+
+					<article id="post-not-found" class="hentry clearfix">
+						<header class="article-header">
+							<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+						</header>
+						<section class="entry-content">
+							<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+						</section>
+						<footer class="article-footer">
+							<p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
+						</footer>
+					</article>
+
+					<?php endif; ?>
+
+					<a href="#top" class="btn btn-primary" role="button">Return to Top</a>
 				</div>
-
 			</div>
-
+		</div>
+	</div>
+</div>
 <?php get_footer(); ?>
