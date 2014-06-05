58c94e2fa0216e402b0849e18d04e365da2b7cfd
Jimmy MG Lim <mirageglobe@gmail.com>
2014-06-05 16:12:06 +0100

    updated logo to generic zen circle image and cleaned up source

--- footer.php
+++ footer.php
@@ -1,27 +1,27 @@
 		<div class="container">
-            <div class="pull-left">
-                <p class="source-org copyright">
-                    &copy; 2013 - <?php echo date('Y'); ?> 
-                    &middot; <i class="fa fa-home"></i> <?php bloginfo( 'name' ); ?> 
-                    &middot; <i class="fa fa-globe"></i> Developed by <a href="http://www.dracoturtur.com">Dracoturtur London</a>
-                </p>
-            </div>
-            <div class="pull-right">
-                <p>
-                    <i class="fa fa-cog "></i> <a href="<?php echo home_url(); ?>/wp-admin">Admin</a>
-                    &middot; <i class="fa fa-rss "></i> <a href="feed">RSS Feed</a>
-                </p>
-            </div>
-        </div>
+      <div class="pull-left">
+        <p class="source-org copyright">
+          &copy; <?php echo date('Y'); ?> 
+          &middot; <i class="fa fa-home"></i> <?php bloginfo( 'name' ); ?>  &middot; <br>
+          <small><i class="fa fa-globe"></i> powered by <a href="https://github.com/mirageglobe/bone">Bone Theme</a>, developed by <a href="http://www.dracoturtur.com">Dracoturtur</a>, London UK </small>
+        </p>
+      </div>
+      <div class="pull-right">
+        <p>
+          <i class="fa fa-cog "></i> <a href="<?php echo home_url(); ?>/wp-admin">Admin</a>
+          &middot; <i class="fa fa-rss "></i> <a href="feed">RSS Feed</a>
+        </p>
+      </div>
+    </div>
 
-        <!------------------------------------------------- 
-        Bootstrap core JavaScript
-	    -------------------------------------------------->
+    <!------------------------------------------------- 
+      Bootstrap core JavaScript
+    -------------------------------------------------->
 
-	    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- Placed at the end of the document so the pages load faster -->
 
-	    <script src="//cdn.jsdelivr.net/jquery/2.1.1/jquery.min.js"></script>
-	    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.2/js/bootstrap.min.js"></script>
+    <script src="//cdn.jsdelivr.net/jquery/2.1.1/jquery.min.js"></script>
+    <script src="//cdn.jsdelivr.net/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
 		<?php wp_footer(); ?>
 	</body>
--- header.php
+++ header.php
@@ -8,51 +8,51 @@
 <head>
 	<meta charset="utf-8">
 
-	<?php // Google Chrome Frame for IE ?>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <?php // Google Chrome Frame for IE ?>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title><?php wp_title(''); ?></title>
+  <title><?php wp_title(''); ?></title>
 
-	<?php // mobile meta (hooray!) ?>
-	<meta name="HandheldFriendly" content="True">
-	<meta name="MobileOptimized" content="320">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <?php // mobile meta (hooray!) ?>
+  <meta name="HandheldFriendly" content="True">
+  <meta name="MobileOptimized" content="320">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-	<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
-	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-	<!--[if IE]>
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-	<![endif]-->
-	<?php // or, set /favicon.ico for IE10 win ?>
-	<meta name="msapplication-TileColor" content="#f01d4f">
-	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+  <?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
+  <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
+  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+  <!--[if IE]>
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+  <![endif]-->
+  <?php // or, set /favicon.ico for IE10 win ?>
+  <meta name="msapplication-TileColor" content="#f01d4f">
+  <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
 
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-	<link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">
-	<link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.2/css/bootstrap-theme.min.css" rel="stylesheet">
-	<link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
-    
-    <link href="//netdna.bootstrapcdn.com/bootswatch/3.0.3/spacelab/bootstrap.min.css" rel="stylesheet">
+  <link href="//cdn.jsdelivr.net/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+  <link href="//cdn.jsdelivr.net/bootstrap/3.1.1/css/bootstrap-theme.css" rel="stylesheet">
+  <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
+
+  <link href="//cdn.jsdelivr.net/bootswatch/3.1.1.1/spacelab/bootstrap.min.css" rel="stylesheet">
     
 	<style type="text/css">
 	body {
 		padding-top: 20px;
-        background: transparent;
-        }
-    html{
-        background: url("<?php echo get_template_directory_uri(); ?>/library/images/background.png") no-repeat center center fixed;
-        -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover;
-        background-size: cover;
+      background: transparent;
+      }
+    html {
+      background: url("<?php echo get_template_directory_uri(); ?>/library/images/background.png") no-repeat center center fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
 	   }
 	</style>
 
-    <?php // wordpress head functions ?>
+  <!-- // wordpress head functions -->
 	<?php wp_head(); ?>
-	<?php // end of wordpress head ?>
+	<!-- // end of wordpress head -->
 </head>
 
 <body <?php body_class(); ?>>
@@ -81,7 +81,7 @@
 					<?php 
 
 					$menubarpill = wp_page_menu(array(
-				        'depth'       => 0,
+				    'depth'       => 0,
 						'sort_column' => 'menu_order, post_title',
 						'menu_class'  => 'menu',
 						'include'     => '',
