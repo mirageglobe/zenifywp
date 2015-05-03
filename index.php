abb67807eef6fc3bd0ea22c2789297d4fd56e451
Jimmy MG Lim <mirageglobe@gmail.com>
2015-05-03 11:23:01 +0100

    added splash configurable carousel

--- index.php
+++ index.php
@@ -65,38 +65,10 @@
       <!-- start top slider -->
       <?php if(is_front_page()): ?>
       
-      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        </ol>
-        
-        <div class="carousel-inner">
-          <div class="item active">
-           <img src="<?php echo get_template_directory_uri(); ?>/library/images/splash01.png" alt="bone">
-            <div class="carousel-caption">
-              <?php //bloginfo( 'name' ); ?>
-            </div>
-          </div>
-          <div class="item">
-            <img src="<?php echo get_template_directory_uri(); ?>/library/images/splash02.png" alt="bone">
-            <div class="carousel-caption">
-              <?php //bloginfo( 'name' ); ?>
-            </div>
-          </div>
-        </div>
-
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left"></span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right"></span>
-        </a>
-      </div>
-      
-      <br>
+      <?php
+        get_template_part( 'splash' );
+        // get the content splash template file. same as include
+      ?>
       
       <?php endif; ?>
       <!-- end top slider -->
