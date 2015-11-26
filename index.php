e0a9b70fc6e0a6daec750e2eb5771c74101bce6e
Jimmy MG Lim <mirageglobe@gmail.com>
2015-11-26 16:02:42 +0000

    updated roadmap

--- index.php
+++ index.php
@@ -12,48 +12,10 @@
 
     <div class="container">
 
-      <!-- start top nav supports mobile -->
-      <nav class="navbar navbar-default" role="navigation">
-
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bsnavbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-
-          <a class="navbar-brand" href="<?php echo home_url(); ?>" rel="nofollow">
-            <img src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" class="img-rounded pull-left" width="20px">&nbsp;
-            <?php bloginfo( 'name' ); ?>
-          </a>
-        </div>
-
-        <!-- top nav collapse -->
-        <div class="collapse navbar-collapse" id="bsnavbar-collapse">
-        <?php 
-
-        $menubarpill = wp_page_menu(array(
-          'depth'       => 0,
-          'sort_column' => 'menu_order, post_title',
-          'menu_class'  => 'menu',
-          'include'     => '',
-          'exclude'     => '',
-          'echo'        => false,
-          'show_home'   => false,
-          'link_before' => '',
-          'link_after'  => '' 
-        )); 
-
-        $menubarpill = str_replace('<ul>', '<ul class="nav navbar-nav navbar-right">', $menubarpill);
-        $menubarpill = str_replace("<ul class='children'>", '<ul class="dropdown-menu">', $menubarpill);
-
-        echo $menubarpill;
-        ?>
-        </div>
-      </nav>
-      <!-- end top nav -->
+      <?php
+        get_template_part( 'menuboxtop' );
+        // get the template file. same as include.
+      ?>
 
       <!-- search form -->
       <div class="pull-right texttiny zen_searchform">
@@ -216,8 +178,8 @@
       <!-- optional lowermenu content -->
       <div class="col-md-12">
       <?php
-        get_template_part( 'lowermenu' );
-        // get the content splash template file. same as include
+        get_template_part( 'menuboxbottom' );
+        // get the template file. same as include.
       ?>
       </div>
       <!-- end lowermenu content -->
