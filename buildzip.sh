3b744feec9b2f21b82f4ab4eac7b90831e65bd42
Jimmy MG Lim <mirageglobe@gmail.com>
2016-08-21 20:59:46 +0100

    fixed zipping issue with git zip

--- buildzip.sh
+++ buildzip.sh
@@ -1,4 +1,5 @@
 #/usr/bin/env bash
 
 echo "[+] Compressing for Wordpress"
-zip -r -X zenifywordpress.zip .
+#zip -r -X -9 -x *.git* zenifywordpress.zip .
+git archive --format=zip HEAD -o zenifywordpress.zip
