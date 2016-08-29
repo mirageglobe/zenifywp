0bd2234b9ef823955783f615339d3ba981b6a823
Jimmy MG Lim <mirageglobe@gmail.com>
2016-08-29 19:29:17 +0100

    check if have posts

--- menuboxbottom.php
+++ menuboxbottom.php
@@ -1,7 +1,9 @@
 <!-- This block is just above footer -->
 
+<?php if (have_posts()) : ?>
 <br>
 <div>
-  <div class="texttiny bold">Recent Posts and Pages</div>
+  <div class="texttiny bold">Recent Posts</div>
   <div class="texttiny"><?php wp_get_archives( array( 'type' => 'postbypost', 'before' => ' ', 'after' => ' &middot;', 'limit' => 500, 'format' => 'custom' ) ); ?></div>
 </div>
+<?php endif; ?>
