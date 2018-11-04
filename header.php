0f1df60fade04efc93b8d55beebc11670f04fb8f
Jimmy MG Lim <mirageglobe@gmail.com>
2018-11-04 15:19:40 +0000

    removed favicons

--- header.php
+++ header.php
@@ -7,14 +7,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
-<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
 
 <!--[if IE]>
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 <![endif]-->
-
-<meta name="msapplication-TileColor" content="#f01d4f">
-<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
 
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 <link rel="profile" href="http://gmpg.org/xfn/11" />
