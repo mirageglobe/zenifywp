aece752da100dba822887110e38b93d04959418f
Jimmy MG Lim <mirageglobe@users.noreply.github.com>
2019-05-22 14:37:58 +0100

    Update buildzip.sh

--- buildzip.sh
+++ buildzip.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 echo "[+] Compressing for wordpress themes"
 #zip -r -X -9 -x *.git* zenifywp.zip .
 
 #git archive automatically ignores git folder as well as any gitignored files
-git archive --format=zip HEAD -o zenifywp.zip
+git archive --format=zip HEAD:src/ -o zenifywp.zip
